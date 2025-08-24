5a5fa3bbff90d7aa96992f16cce949f149ae2fe0
tanjimN4 <tanjimahemed7@gmail.com>
2025-08-24 12:03:55 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <div><Button>Button</Button></div>
+    </>
+  )
+}
+
+export default App
